248ce245c8da31b2cf943a8e29230719e50aa229
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-03-31 09:03:34 -0700

    add csproj file and Parks model

---
+++ ParksLookupApi/Models/Parks.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ParksLookupApi.Models;
+{
+  public class Parks
+  {
+    public int ParksId { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string ParkName { get; set; }
+
+    [Required]
+    [StateOrNational(ErrorMessage = "Type must be 'State' or 'National'")]
+      public string Type { get; set; }
+  }
+
+  //custom validation for Type to be 'State' or 'National' only
+  public class StateOrNationalAttribute : ValidationAttribute
+  {
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      var type = value as string;
+      if (type == null || (type.ToLower() != "state" && type.ToLower() != "national"))
+      {
+        return new ValidationResult($"{validationContext.DisplayName} must be 'State' or 'National'");
+      }
+      return ValidationResult.Success;
+    }
+  }
+}
